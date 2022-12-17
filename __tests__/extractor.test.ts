b7242317873e2c61d32ae682de204d81f0c81769
Paul Schuberth <github@pschuberth.de>
2022-12-17 23:50:09 +0100

    Add extraction modes `all`, `unique`, and `first`
    Mode `all` returns all matches that are found for the given combination
    of haystack and needle.
    
    Mode `unique` depuplicates the matches that are found for the given
    combination of haystack and needle.
    
    Mode `first` returns only the first match that is found.
    
    All modes return an empty list if no matches are found.

--- __tests__/extractor.test.ts
+++ __tests__/extractor.test.ts
@@ -1,6 +1,7 @@
-import {extractor} from '../src/extractor'
+import {extractor, JIRA_ISSUE, Mode} from '../src/extractor'
 import {expect, test, beforeEach} from '@jest/globals'
 import {readFileSync} from 'fs'
+import { it } from 'node:test'
 
 const core = require('@actions/core')
 
@@ -12,11 +13,11 @@ test('finds jira issues by default', async () => {
 
 test('can be given different regexp', async () => {
   const content = readFileSync('resources/single.txt', 'utf-8')
-  const actual = await extractor(content, /\d+/gm)
+  const actual = await extractor(content, {needle: /\d+/gm})
   expect(actual).not.toEqual(['ABC-23'])
 })
 
-test('deduplicates matches', async () => {
+test('deduplicates matches by default', async () => {
   const content = readFileSync('resources/multi.txt', 'utf-8')
   const actual = await extractor(content)
   expect(actual).toContain('ABC-123')
@@ -24,3 +25,33 @@ test('deduplicates matches', async () => {
   expect(actual).toContain('ABC-42')
   expect(actual).toHaveLength(3)
 })
+
+test('returns empty list for no matches in first mode', async () => {
+  const content = readFileSync('resources/single.txt', 'utf-8')
+  const actual = await extractor(content, {needle: /NOMATCH/gm, mode: "first"})
+  expect(actual).toHaveLength(0)
+  expect(actual).toBeInstanceOf(Array)
+});
+
+test('returns empty list for no matches in all mode', async () => {
+  const content = readFileSync('resources/single.txt', 'utf-8')
+  const actual = await extractor(content, {needle: /NOMATCH/gm, mode: "all"})
+  expect(actual).toHaveLength(0)
+  expect(actual).toBeInstanceOf(Array)
+});
+
+test('returns empty list for no matches in unique mode', async () => {
+  const content = readFileSync('resources/single.txt', 'utf-8')
+  const actual = await extractor(content, {needle: /NOMATCH/gm, mode: "unique"})
+  expect(actual).toHaveLength(0)
+  expect(actual).toBeInstanceOf(Array)
+});
+
+
+test('can return only the first match', async () => {
+  const content = readFileSync('resources/multi.txt', 'utf-8')
+  const actual = await extractor(content, {mode: "first"})
+  expect(actual).toContain('ABC-123')
+  expect(actual).toHaveLength(1)
+});
+
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import * as core from '@actions/core'
-import {extractor} from './extractor'
+import {extractor, JIRA_ISSUE, Mode} from './extractor'
 
 export async function run(): Promise<void> {
   try {
     const haystack: string = core.getInput('haystack')
     let customNeedle: string = core.getInput('needle')
 
-    let matches: string[]
+    let mode = core.getInput('mode') as Mode
+
+    let matches: string[]|string
     if (!customNeedle) {
       customNeedle = '[A-Z]+-d'
-      matches = await extractor(haystack)
+      matches = await extractor(haystack, { mode: mode })
     } else {
-      matches = await extractor(haystack, new RegExp(customNeedle))
+      matches = await extractor(haystack, {needle: new RegExp(customNeedle), mode: mode })
     }
 
     core.setOutput('matches', matches)
